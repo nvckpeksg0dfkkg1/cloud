92a4b55d4016e8d6d005ecc446eaed28c5462c56
nvckpeksg0dfkkg1 <103400762+nvckpeksg0dfkkg1@users.noreply.github.com>
2022-12-06 11:09:40 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=68089927-f086-4d18-befb-49baa4b998aa
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=brito.id
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
